0eca130a397ff05d2887e9827813bea5f3225a03
Ralph-Dulla-23 <142960747+Ralph-Dulla-23@users.noreply.github.com>
2024-03-26 10:28:40 +0800

    Add files via upload

---
+++ Machine Problems/mp5.asm
@@ -0,0 +1,66 @@
+section .data
+    newline db 10
+
+section .bss
+    buffer resb 1
+
+section .text
+    global _start
+
+_start:
+    ; Read a character from stdin
+    mov eax, 3          
+    mov ebx, 0           
+    mov ecx, buffer     
+    mov edx, 1          
+    int 0x80             
+
+    cmp byte [buffer], 'A'
+    jl skip_program
+
+    cmp byte [buffer], 'z'
+    jg skip_program
+
+
+if_uppercase:
+
+    cmp byte [buffer], 'Z'
+    jg if_lowercase
+
+    add byte [buffer], 32
+
+    cmp byte [buffer], '`'
+    jg skip_program
+
+   
+
+
+if_lowercase: 
+
+    cmp byte [buffer], 'a'
+    jl skip_program
+
+    cmp byte [buffer], 'z'
+    jg skip_program    
+
+    sub byte [buffer], 32
+
+
+skip_program:
+
+    mov eax, 4           
+    mov ebx, 1          
+    mov ecx, buffer     
+    mov edx, 1          
+    int 0x80     
+
+    mov eax, 4           
+    mov ebx, 1          
+    mov ecx, newline    
+    mov edx, 1          
+    int 0x80          
+
+    ; Exit the program
+    mov eax, 1           
+    xor ebx, ebx        
+    int 0x80       
